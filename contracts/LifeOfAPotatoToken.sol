da8997f9086a9a6737c2841b4f7cef6b94385704
0xjessel <github@jessechen.net>
2021-12-23 11:38:29 -0800

    visual studio formatting

--- contracts/LifeOfAPotatoToken.sol
+++ contracts/LifeOfAPotatoToken.sol
@@ -13,39 +13,41 @@ contract LifeOfAPotatoToken is ERC721URIStorage, Ownable {
 
     // get the current supply of tokens
     function totalSupply() public view returns (uint256) {
-      return _tokenIds.current();
+        return _tokenIds.current();
     }
 
-    // for opensea collection 
+    // for opensea collection
     function contractURI() public pure returns (string memory) {
-      return "https://ipfs.io/ipfs/your-collection-ipfshash";
+        return "https://ipfs.io/ipfs/your-collection-ipfshash";
     }
 
     function mintItem(address receiver, string memory tokenURI)
-      public
-      onlyOwner
-      returns (uint256)
+        public
+        onlyOwner
+        returns (uint256)
     {
-      _tokenIds.increment();
-      uint256 newPotatoID = _tokenIds.current();
-      _mint(receiver, newPotatoID);
-      _setTokenURI(newPotatoID, tokenURI);
-      return newPotatoID;
+        _tokenIds.increment();
+        uint256 newPotatoID = _tokenIds.current();
+        _mint(receiver, newPotatoID);
+        _setTokenURI(newPotatoID, tokenURI);
+        return newPotatoID;
     }
 
     /**
      * Override isApprovedForAll to auto-approve OS's proxy contract
      */
-    function isApprovedForAll(
-      address _owner,
-      address _operator
-    ) public override view returns (bool isOperator) {
-      // if OpenSea's ERC721 Proxy Address is detected, auto-return true
-      if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
-        return true;
-      }
-        
-      // otherwise, use the default ERC721.isApprovedForAll()
-      return ERC721.isApprovedForAll(_owner, _operator);
+    function isApprovedForAll(address _owner, address _operator)
+        public
+        view
+        override
+        returns (bool isOperator)
+    {
+        // if OpenSea's ERC721 Proxy Address is detected, auto-return true
+        if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
+            return true;
+        }
+
+        // otherwise, use the default ERC721.isApprovedForAll()
+        return ERC721.isApprovedForAll(_owner, _operator);
     }
 }
