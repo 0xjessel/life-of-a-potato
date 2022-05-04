1bd7b2cbc6a97f5c0c4a25caf936248c4599005d
Long Tran <ltran.co8@gmail.com>
2022-05-03 23:24:26 -0700

    bulk script fixes

--- scripts/opensea-bulk.js
+++ scripts/opensea-bulk.js
@@ -1,3 +1,4 @@
+const MNEMONIC = process.env.MNEMONIC;
 const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
 const INFURA_KEY = process.env.INFURA_KEY;
 const OPEN_SEA_API_KEY = process.env.OPEN_SEA_API_KEY || "";
@@ -9,14 +10,20 @@ if (!FACTORY_CONTRACT_ADDRESS) {
 const opensea = require("opensea-js");
 const RPCSubprovider = require("web3-provider-engine/subproviders/rpc");
 const Web3ProviderEngine = require("web3-provider-engine");
+const MnemonicWalletSubprovider = require("@0x/subproviders").MnemonicWalletSubprovider;
 
 const OpenSeaPort = opensea.OpenSeaPort;
 const Network = opensea.Network;
 
+const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
 const network = "rinkeby";
 const infuraRpcSubprovider = new RPCSubprovider({
   rpcUrl: `https://${network}.infura.io/v3/${INFURA_KEY}`
 });
+const mnemonicWalletSubprovider = new MnemonicWalletSubprovider({
+    mnemonic: MNEMONIC,
+    baseDerivationPath: BASE_DERIVATION_PATH,
+  });
 
 const providerEngine = new Web3ProviderEngine();
 providerEngine.addProvider(mnemonicWalletSubprovider);
@@ -33,15 +40,19 @@ const seaport = new OpenSeaPort(
 );
 
 
-const expirationTime = (Date.now() / 1000 + 60 * 60 * 24 * 365) // Expires 1 year from now
+const expirationTime = (Date.now() / 1000 + 60 * 60 * 24 * 180) // Expires 6mo from now
 
-const sellOrders = await seaport.createFactorySellOrders({
-  assetId: "1",
-  factoryAddress: FACTORY_CONTRACT_ADDRESS,
-  accountAddress,
-  startAmount,
-  expirationTime,
-  numberOfOrders: 100 // Will create 100 sell orders in parallel batches to speed things up
-})
+const sell = async () => {
+    const sellOrders = await seaport.createFactorySellOrders({
+        assetId: "1",
+        factoryAddress: FACTORY_CONTRACT_ADDRESS,
+        accountAddress,
+        startAmount,
+        expirationTime,
+        numberOfOrders: 100 // Will create 100 sell orders in parallel batches to speed things up
+      })
+      
+      console.log(sellOrders)
+}
 
-console.log(sellOrders)
+sell()
